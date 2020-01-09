f6bf931e5b88540a9524f8464377a6f88c395a80
Alex Aranda <alexraranda@gmail.com>
2020-01-08 23:23:34 -0800

    index.js adapted to express.js, get and post method complete and verified via httpie

--- server/index.js
+++ server/index.js
@@ -1,14 +1,57 @@
-const path = require('path');
-const jsonServer = require('json-server');
+const express = require('express');
+const app = express();
+const fs = require('fs');
+const gradesObj = require('../database/data.json');
 
-const dbPath = path.resolve(__dirname, '../database/db.json');
-const server = jsonServer.create();
-const middleware = jsonServer.defaults();
-const endpoints = jsonServer.router(dbPath);
+const errors = [
+  { Error: 'Can not find note with that id' },
+  { Error: 'Id must be positive integer' },
+  { Error: 'An unexpected error occurred.' },
+  { Error: 'content is a required field' },
+  { Error: 'An unexpected error occurred' }
+];
 
-server.use(middleware);
-server.use('/api', endpoints);
-server.listen(3001, () => {
-  // eslint-disable-next-line no-console
-  console.log('JSON Server listening on port 3001\n');
+app.get('/api/grades', (req, res) => {
+  res.json(gradesObj);
 });
+
+app.use(express.json());
+
+app.post('/api/grades', (req, res) => {
+  const nextId = gradesObj.nextId - 1;
+  gradesObj.grades.push(req.body);
+  gradesObj.grades[nextId].id = gradesObj.nextId;
+  fs.writeFile(
+    '../database/data.json',
+    JSON.stringify(gradesObj, null, 2),
+    err => {
+      if (err) {
+        res.status(500).send(errors[4]);
+        console.error(err);
+      } else {
+        res.status(201).send(gradesObj.grades[nextId]);
+        gradesObj.nextId++;
+      }
+    });
+
+});
+
+app.listen(3000, () => {
+  // eslint-disable-next-line no-console
+  console.log('Express Server Listening on Port 3000!');
+});
+
+// const path = require('path');
+// const jsonServer = require('json-server');
+
+// const dbPath = path.resolve(__dirname, '../database/db.json');
+// const server = jsonServer.create();
+// const middleware = jsonServer.defaults();
+// const endpoints = jsonServer.router(dbPath);
+
+// server.use(middleware);
+// server.use('/api', endpoints);
+// server.listen(3001, () => {
+//   // eslint-disable-next-line no-console
+//   console.log('JSON Server listening on port 3001\n');
+// });
